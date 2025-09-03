782809939825c17b07c100f67a70fc465bc5d975
MichaelBoegner <cmikeb@gmail.com>
2025-09-03 16:13:45 +0700

    added database connection and table users for login mgmt

--- app/database.py
+++ app/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 
-load_dotenv()  # reads .env file
+load_dotenv()  
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
