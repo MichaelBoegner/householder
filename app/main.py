75923c2d2165d775387d012bb450e7999ef1ef97
MichaelBoegner <cmikeb@gmail.com>
2025-09-03 16:13:33 +0700

    added database connection and table users for login mgmt

--- app/main.py
+++ app/main.py
@@ -1,6 +1,20 @@
-from fastapi import FastAPI
-from app.api import meditate
+# main.py
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from app import models
+from app.database import engine, SessionLocal, Base
 
 app = FastAPI()
 
-app.include_router(meditate.router)
+Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.get("/users")
+def list_users(db: Session = Depends(get_db)):
+    return db.query(models.User).all()
